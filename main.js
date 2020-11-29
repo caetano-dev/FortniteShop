bdc75431d0f2768ac986ba1b30b7c5ef8b68caea
Pedro Pires <pcaetanop@gmail.com>
2020-11-28 21:31:13 -0300

    Refactoring

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-  win.loadFile("shop.html");
+  win.loadFile("./public/shop.html");
   win.once("ready-to-show", () => {
     win.show();
   });
